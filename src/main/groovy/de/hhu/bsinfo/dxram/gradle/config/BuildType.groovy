02396a4550957175caa81d85c50c4c6ff75447cd
Stefan Nothaas <stefan.nothaas@hhu.de>
2018-07-18 08:31:26 +0200

    BuildType: adapt excludeStatistics to excludeLogger

--- src/main/groovy/de/hhu/bsinfo/dxram/gradle/config/BuildType.groovy
+++ src/main/groovy/de/hhu/bsinfo/dxram/gradle/config/BuildType.groovy
@@ -47,36 +47,70 @@ class BuildType {
         }
     }
 
-    void excludeStatistics() {
+    @SuppressWarnings("GroovyFallthrough")
+    void excludeStatistics(String level) {
+        switch (level) {
+            case LEVEL_PERFORMANCE:
+                excludeInvocations 'de.hhu.bsinfo.dxutils.stats.Throughput#start'
+                excludeInvocations 'de.hhu.bsinfo.dxutils.stats.Throughput#add'
+                excludeInvocations 'de.hhu.bsinfo.dxutils.stats.Throughput#stop'
 
-        excludeInvocations 'de.hhu.bsinfo.dxutils.stats.Throughput#startDebug'
-        excludeInvocations 'de.hhu.bsinfo.dxutils.stats.Throughput#addDebug'
-        excludeInvocations 'de.hhu.bsinfo.dxutils.stats.Throughput#stopDebug'
+                excludeInvocations 'de.hhu.bsinfo.dxutils.stats.ThroughputPool#start'
+                excludeInvocations 'de.hhu.bsinfo.dxutils.stats.ThroughputPool#add'
+                excludeInvocations 'de.hhu.bsinfo.dxutils.stats.ThroughputPool#stop'
 
-        excludeInvocations 'de.hhu.bsinfo.dxutils.stats.ThroughputPool#startDebug'
-        excludeInvocations 'de.hhu.bsinfo.dxutils.stats.ThroughputPool#addDebug'
-        excludeInvocations 'de.hhu.bsinfo.dxutils.stats.ThroughputPool#stopDebug'
+                excludeInvocations 'de.hhu.bsinfo.dxutils.stats.Time#start'
+                excludeInvocations 'de.hhu.bsinfo.dxutils.stats.Time#stop'
 
-        excludeInvocations 'de.hhu.bsinfo.dxutils.stats.Time#startDebug'
-        excludeInvocations 'de.hhu.bsinfo.dxutils.stats.Time#stopDebug'
+                excludeInvocations 'de.hhu.bsinfo.dxutils.stats.Timeline#start'
+                excludeInvocations 'de.hhu.bsinfo.dxutils.stats.Timeline#nextSection'
+                excludeInvocations 'de.hhu.bsinfo.dxutils.stats.Timeline#stop'
 
-        excludeInvocations 'de.hhu.bsinfo.dxutils.stats.Timeline#startDebug'
-        excludeInvocations 'de.hhu.bsinfo.dxutils.stats.Timeline#nextSectionDebug'
-        excludeInvocations 'de.hhu.bsinfo.dxutils.stats.Timeline#stopDebug'
+                excludeInvocations 'de.hhu.bsinfo.dxutils.stats.TimePercentile#record'
 
-        excludeInvocations 'de.hhu.bsinfo.dxutils.stats.TimePercentile#recordDebug'
+                excludeInvocations 'de.hhu.bsinfo.dxutils.stats.TimePercentilePool#start'
 
-        excludeInvocations 'de.hhu.bsinfo.dxutils.stats.TimePercentilePool#startDebug'
+                excludeInvocations 'de.hhu.bsinfo.dxutils.stats.TimePool#start'
+                excludeInvocations 'de.hhu.bsinfo.dxutils.stats.TimePool#stop'
 
-        excludeInvocations 'de.hhu.bsinfo.dxutils.stats.TimePool#startDebug'
-        excludeInvocations 'de.hhu.bsinfo.dxutils.stats.TimePool#stopDebug'
+                excludeInvocations 'de.hhu.bsinfo.dxutils.stats.Value#inc'
+                excludeInvocations 'de.hhu.bsinfo.dxutils.stats.Value#add'
 
-        excludeInvocations 'de.hhu.bsinfo.dxutils.stats.Value#incDebug'
-        excludeInvocations 'de.hhu.bsinfo.dxutils.stats.Value#addDebug'
+                excludeInvocations 'de.hhu.bsinfo.dxutils.stats.ValuePercentile#record'
 
-        excludeInvocations 'de.hhu.bsinfo.dxutils.stats.ValuePercentile#recordDebug'
+                excludeInvocations 'de.hhu.bsinfo.dxutils.stats.ValuePool#inc'
+                excludeInvocations 'de.hhu.bsinfo.dxutils.stats.ValuePool#add'
 
-        excludeInvocations 'de.hhu.bsinfo.dxutils.stats.ValuePool#incDebug'
-        excludeInvocations 'de.hhu.bsinfo.dxutils.stats.ValuePool#addDebug'
+            case LEVEL_RELEASE:
+                excludeInvocations 'de.hhu.bsinfo.dxutils.stats.Throughput#startDebug'
+                excludeInvocations 'de.hhu.bsinfo.dxutils.stats.Throughput#addDebug'
+                excludeInvocations 'de.hhu.bsinfo.dxutils.stats.Throughput#stopDebug'
+
+                excludeInvocations 'de.hhu.bsinfo.dxutils.stats.ThroughputPool#startDebug'
+                excludeInvocations 'de.hhu.bsinfo.dxutils.stats.ThroughputPool#addDebug'
+                excludeInvocations 'de.hhu.bsinfo.dxutils.stats.ThroughputPool#stopDebug'
+
+                excludeInvocations 'de.hhu.bsinfo.dxutils.stats.Time#startDebug'
+                excludeInvocations 'de.hhu.bsinfo.dxutils.stats.Time#stopDebug'
+
+                excludeInvocations 'de.hhu.bsinfo.dxutils.stats.Timeline#startDebug'
+                excludeInvocations 'de.hhu.bsinfo.dxutils.stats.Timeline#nextSectionDebug'
+                excludeInvocations 'de.hhu.bsinfo.dxutils.stats.Timeline#stopDebug'
+
+                excludeInvocations 'de.hhu.bsinfo.dxutils.stats.TimePercentile#recordDebug'
+
+                excludeInvocations 'de.hhu.bsinfo.dxutils.stats.TimePercentilePool#startDebug'
+
+                excludeInvocations 'de.hhu.bsinfo.dxutils.stats.TimePool#startDebug'
+                excludeInvocations 'de.hhu.bsinfo.dxutils.stats.TimePool#stopDebug'
+
+                excludeInvocations 'de.hhu.bsinfo.dxutils.stats.Value#incDebug'
+                excludeInvocations 'de.hhu.bsinfo.dxutils.stats.Value#addDebug'
+
+                excludeInvocations 'de.hhu.bsinfo.dxutils.stats.ValuePercentile#recordDebug'
+
+                excludeInvocations 'de.hhu.bsinfo.dxutils.stats.ValuePool#incDebug'
+                excludeInvocations 'de.hhu.bsinfo.dxutils.stats.ValuePool#addDebug'
+        }
     }
 }
